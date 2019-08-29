23c030cc91966403a51ce0f07c40a4b89455643e
aa0918358122 <aa0918358122@gmail.com>
2019-08-29 14:23:28 +0800

    main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import listdir
+import numpy as np
 import copy
 import math
 
@@ -11,12 +12,14 @@ if "__main__" == __name__:
     files = listdir('./data/')
     articles = []
     char_set = set()
+    y = []
 
     for file in files:
+        y.append(int(file[0]))
         article = read_file('./data/' + file)
 
         # delete the redundant words in articles
-        deletion = ['，', '。', '、', '「', '」', ' ','；', '（', '）']
+        deletion = ['，', '。', '、', '「', '」', ' ','；', '（', '）', '！', '／', '？', '～']
         for char in deletion:
             article = article.replace(char, '') # str.replace()
 
@@ -43,11 +46,21 @@ if "__main__" == __name__:
         for i in range(len(articles)):
             if count_dict[i][char]:
                 count += 1
-        idf_dict[char] = math.log((10/count), 10)
+        idf_dict[char] = math.log((30/count), 10)
 
     #count tf, tf-idf
     for i in range(len(articles)):
+        # print(i)
         for char in char_set:
             tfidf_dict[i][char] = count_dict[i][char] / len(articles[i]) * idf_dict[char]
 
-    print(tfidf_dict)
+    x = []
+
+    for i, article in enumerate(articles):
+        x.append([tfidf_dict[i][k] for k in sorted(tfidf_dict[i].keys())])
+
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    np.save('x.npy', x)
+    np.save('y.npy', y)
