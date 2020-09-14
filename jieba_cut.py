29cde1960409ca32fd25bc610b049d63707a380c
aa0918358122 <aa0918358122@gmail.com>
2020-09-14 14:58:22 +0800

    change path

--- jieba_cut.py
+++ jieba_cut.py
@@ -4,6 +4,7 @@ import jieba
 import sys
 import numpy as np
 import re
+import pickle
 from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -17,7 +18,8 @@ def read_file(file):
 def classification(number):
     type_list = []
     for i in range(1, number+1):
-        files = listdir(f'/home/aa0918358122/git/crawler/articles_{sys.argv[2]}/{i}/')
+        # files = listdir(f'/home/aa0918358122/git/crawler/articles_{sys.argv[2]}/{i}/')
+        files = listdir(f'./articles_politic/{i}/')
         type_list.append(files)
     return type_list
 
@@ -35,10 +37,10 @@ if __name__ == "__main__":
         for stopword in stopwords:
             stopword_set.add(stopword.strip('\n'))
         stopword_set.add('\n')
-    # ckiptagger
+    # ckiptagger_ws
     ws = WS('./model')
 
-    for i, file in enumerate(classification(7)):
+    for i, file in enumerate(classification(9)):
         count = []
         for j in range(len(file)):
             # print(file[j])
@@ -46,20 +48,21 @@ if __name__ == "__main__":
             # choose specific date before or after some big event
             date = re.findall(r'-.*-', file[j])
             date = datetime.strptime(f'{date[0][1:9]}', '%Y%m%d')
-            start_date = datetime.strptime('20200215', '%Y%m%d')
-            end_date = datetime.strptime('20200625', '%Y%m%d')
+            start_date = datetime.strptime('20200202', '%Y%m%d')
+            end_date = datetime.strptime('20200702', '%Y%m%d')
             duration1 = date - start_date
             duration2 = date - end_date
             if duration1.total_seconds() >= 0 and duration2.total_seconds() <= 0:
 
                 # choose all date
-                article = read_file(f'/home/aa0918358122/git/crawler/articles_{sys.argv[2]}/{i+1}/{file[j]}')
+                # article = read_file(f'/home/aa0918358122/git/crawler/articles_{sys.argv[2]}/{i+1}/{file[j]}')
+                article = read_file(f'./articles_politic/{i+1}/{file[j]}')
                 count.append(int(file[j][0]))
                 y.append(int(file[j][0]))
                 # use jieba
                 if sys.argv[1] == 'jieba':
                     seg_list = jieba.cut(article, cut_all = False)
-                # use ckiptagger
+                # use ckiptagger_ws
                 elif sys.argv[1] == 'ckiptagger':
                     seg_list = ws([article])[0]
                 res = []
@@ -69,16 +72,24 @@ if __name__ == "__main__":
                 text = ' '.join(res)
                 corpus.append(text)
                 # choose same numbers of articles from each newspaper office
-                if len(count) >= 100:
+                if len(count) >= 500:
                     break
         # print(len(count))
     # print(corpus)
 
 
-    # find tfidf by sklearn
-    vectorizer = CountVectorizer()
-    transformer = TfidfTransformer()
-    tfidf = transformer.fit_transform(vectorizer.fit_transform(corpus))
+    # usee corpus to find tfidf of article by sklearn
+    vectorizer = CountVectorizer(decode_error='replace') # 'replace' must be added to sa
+    tfidftransformer = TfidfTransformer()
+    # training data : vectorizer.fit_transform、tfidftransformer.fit_transform
+    # testing data : vectorizer.transform、tfidftransformer.transform
+    tfidf = tfidftransformer.fit_transform(vectorizer.fit_transform(corpus))
+    # save the form of training data
+    with open('/home/aa0918358122/git/tfidf/feature.pickle', 'wb') as fw:
+        pickle.dump(vectorizer.vocabulary_, fw)
+    with open('/home/aa0918358122/git/tfidf/tfidftransformer.pickle', 'wb') as fw:
+        pickle.dump(tfidftransformer, fw)
+
     word = vectorizer.get_feature_names() #type(word) = list
     weight = tfidf.toarray() # type(weight) = nparray
 
@@ -88,7 +99,7 @@ if __name__ == "__main__":
     feature_array = np.array(word)
     tfidf_sorting_through_index = np.argsort(-total_weight)
     tfidf_sorting_through_value = total_weight[np.argsort(-total_weight)]
-    n = 200
+    n = 20
     top_n = feature_array[tfidf_sorting_through_index][:n] #type(top_n) = nparray
     # least_n = feature_array[tfidf_sorting_through_index][n:]
     # print(top_n)
@@ -104,10 +115,10 @@ if __name__ == "__main__":
         x.append(weight[i])
     # print(len(dimension))
 
-
+    # print(len(y))
     np.save(f'{sys.argv[1]}_{sys.argv[2]}_x.npy', np.asarray(x))
     np.save(f'{sys.argv[1]}_{sys.argv[2]}_y.npy', np.asarray(y))
 
-    np.save(f'{sys.argv[1]}_{sys.argv[2]}_a.npy', np.asarray(a))
-    np.save(f'{sys.argv[1]}_{sys.argv[2]}_topn.npy', top_n)
+    # np.save(f'{sys.argv[1]}_{sys.argv[2]}_a.npy', np.asarray(a))
+    # np.save(f'{sys.argv[1]}_{sys.argv[2]}_topn.npy', top_n)
 
